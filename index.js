3946fc71edf1c3c856ff63fdbf5f3231f8dea37d
tasnimabedeen <99178083+tasnimabedeen@users.noreply.github.com>
2023-11-19 21:04:28 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function change1() {
+    document.getElementById("container3").style.display = "none";
+    document.getElementById("container1").style.display = "none";
+    document.getElementById("container2").style.display = "block";
+ 
+}
+
+function change2() {
+    document.getElementById("container3").style.display = "block";
+    document.getElementById("container1").style.display = "none";
+    document.getElementById("container2").style.display = "none";
+}
+
+function change3() {
+    document.getElementById("container3").style.display = "none";
+    document.getElementById("container1").style.display = "block";
+    document.getElementById("container2").style.display = "none";
+}
