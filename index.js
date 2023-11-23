41d4ce1a0dc7ed1b01d69fe1bcf9a4bd5841e0b0
tasnimabedeen <99178083+tasnimabedeen@users.noreply.github.com>
2023-11-23 22:48:20 +0600

    added text
    -bug fixes related to circles
    -added text with animation
    -one image change for better compatibility

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ function change1() {
     document.getElementById('in22').style.zIndex = "100";
     document.getElementById('in32').style.zIndex = "1";
 
-    document.getElementById("container2").style.animation = "conani2 .5s ease-in-out 1 forwards";
-    document.getElementById("container1").style.animation = "conani .5s ease-in-out 1 forwards";
+    document.getElementById("container2").style.animation = "conani2 1s ease-in-out 1 forwards";
+    document.getElementById("container1").style.animation = "conani 1s ease-in-out 1 forwards";
 
-    document.getElementById('div2').style.animation = "divani1 1.2s ease-in-out 1 forwards";
-    document.getElementById('in21').style.animation = "inani11 1.2s ease-in-out 1 forwards";
-    document.getElementById('in211').style.animation = "inani111 1.2s ease-in-out 1";
+    document.getElementById('div2').style.animation = "divani1 2s ease-in-out 1 forwards";
+    document.getElementById('in21').style.animation = "inani11 2s ease-in-out 1 forwards";
+    document.getElementById('in211').style.animation = "inani111 2s ease-in-out 1";
 
-    document.getElementById('div1').style.animation = "divani2 1.2s ease-in-out 1";
-    document.getElementById('in11').style.animation = "inani12 1.2s ease-in-out 1";
-    document.getElementById('in111').style.animation = "inani211 1.2s ease-in-out 1";
+    document.getElementById('div1').style.animation = "divani2 2s ease-in-out 1";
+    document.getElementById('in11').style.animation = "inani12 2s ease-in-out 1";
+    document.getElementById('in111').style.animation = "inani211 2s ease-in-out 1";
+
+    document.getElementById('text1').style.animation = "textout 2s ease-in-out 1 forwards";
+    document.getElementById('text2').style.animation = "textin 2s ease-in-out 1 forwards";
 }
 
 function change2() {
@@ -24,16 +27,19 @@ function change2() {
     document.getElementById('in22').style.zIndex = "1";
     document.getElementById('in32').style.zIndex = "100";
 
-    document.getElementById("container3").style.animation = "conani2 .5s ease-in-out 1 forwards";
-    document.getElementById("container2").style.animation = "conani .5s ease-in-out 1 forwards";
+    document.getElementById("container3").style.animation = "conani2 1s ease-in-out 1 forwards";
+    document.getElementById("container2").style.animation = "conani 1s ease-in-out 1 forwards";
 
-    document.getElementById('div3').style.animation = "divani1 1.2s ease-in-out 1 forwards";
-    document.getElementById('in31').style.animation = "inani11 1.2s ease-in-out 1 forwards";
-    document.getElementById('in311').style.animation = "inani111 1.2s ease-in-out 1";
+    document.getElementById('div3').style.animation = "divani1 2s ease-in-out 1 forwards";
+    document.getElementById('in31').style.animation = "inani11 2s ease-in-out 1 forwards";
+    document.getElementById('in311').style.animation = "inani111 2s ease-in-out 1";
 
-    document.getElementById('div2').style.animation = "divani2 1.2s ease-in-out 1";
-    document.getElementById('in21').style.animation = "inani12 1.2s ease-in-out 1";
-    document.getElementById('in211').style.animation = "inani211 1.2s ease-in-out 1";
+    document.getElementById('div2').style.animation = "divani2 2s ease-in-out 1";
+    document.getElementById('in21').style.animation = "inani12 2s ease-in-out 1";
+    document.getElementById('in211').style.animation = "inani211 2s ease-in-out 1";
+
+    document.getElementById('text2').style.animation = "textout 2s ease-in-out 1 forwards";
+    document.getElementById('text3').style.animation = "textin 2s ease-in-out 1 forwards";
 }
 
 function change3() {
@@ -43,14 +49,17 @@ function change3() {
     document.getElementById('in22').style.zIndex = "1";
     document.getElementById('in32').style.zIndex = "1";
 
-    document.getElementById("container1").style.animation = "conani2 .5s ease-in-out 1 forwards";
-    document.getElementById("container3").style.animation = "conani .5s ease-in-out 1 forwards";
+    document.getElementById("container1").style.animation = "conani2 1s ease-in-out 1 forwards";
+    document.getElementById("container3").style.animation = "conani 1s ease-in-out 1 forwards";
 
-    document.getElementById('div1').style.animation = "divani1 1.2s ease-in-out 1 forwards";
-    document.getElementById('in11').style.animation = "inani11 1.2s ease-in-out 1 forwards";
-    document.getElementById('in111').style.animation = "inani111 1.2s ease-in-out 1";
+    document.getElementById('div1').style.animation = "divani1 2s ease-in-out 1 forwards";
+    document.getElementById('in11').style.animation = "inani11 2s ease-in-out 1 forwards";
+    document.getElementById('in111').style.animation = "inani111 2s ease-in-out 1";
 
-    document.getElementById('div3').style.animation = "divani2 1.2s ease-in-out 1";
-    document.getElementById('in31').style.animation = "inani12 1.2s ease-in-out 1";
-    document.getElementById('in311').style.animation = "inani211 1.2s ease-in-out 1";
+    document.getElementById('div3').style.animation = "divani2 2s ease-in-out 1";
+    document.getElementById('in31').style.animation = "inani12 2s ease-in-out 1";
+    document.getElementById('in311').style.animation = "inani211 2s ease-in-out 1";
+
+    document.getElementById('text3').style.animation = "textout 2s ease-in-out 1 forwards";
+    document.getElementById('text1').style.animation = "textin 2s ease-in-out 1 forwards";
 }
